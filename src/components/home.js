8235f859031e056155f9f92b7a11fa3d3938ab1f
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-04-04 17:06:24 +0100

    Minor CSS changes

--- src/components/home.js
+++ src/components/home.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <section className="home-section" id="home">
-            <h1>HEY THERE, I'M KAPIL</h1>
-            <br></br>
-            <h1>WELCOME TO MY WEBSITE</h1>
+            <h1>Welcome to the desert of the real</h1>
         </section>
     )
 }

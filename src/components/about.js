e7546798accbc16bec1549ab584061170854ec2d
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-04-04 18:16:56 +0100

    More CSS

--- src/components/about.js
+++ src/components/about.js
@@ -9,7 +9,7 @@ const About = () => {
                 <img src={profilePhoto}/>
                 <div className="about-content">
                     <h2>"There is a difference between knowing the path and walking the path"</h2>
-                    <p>Hello. I'm Kapil, an aspiring software developer from New Zealand, currently based in London. I've always had an interest in software engineering, but only recently have I decided to take the leap and try and forge out a career doing something I love. Take a look around and let me know what you think!</p>
+                    <p>Hello. I'm Kapil, an aspiring software developer from New Zealand, currently based in London. I've always had a bit of an interest in software engineering, but only recently have I decided to take the leap and try and forge out a career doing something I love. Take a look around and let me know what you think!</p>
                     <p>When I'm not involved in tech, you can usually find me playing the guitar or annoying my girlfriend. Two of life's greatest pleasures.</p>
                 </div>
             </div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <h1>THIS IS A FOOTER</h1>
+            <h1>FOOTER</h1>
         </footer>
     )
 }

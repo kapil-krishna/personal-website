a8c0b71b6fc4f2563394fe93e1f8550abc6a5ef6
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-04-06 18:50:24 +0100

    Adding contact form; testing with Netlify

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const Contact = () => {
     return (
         <section className="contact-section" id="contact">
+
+            <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+                <input type="text" name="name" />
+                <input type="email" name="email" />
+                <textarea name="message"></textarea>
+            </form>
+
             <h1>CONTACT</h1>
+            <div className="contact-container">
+                <h2>Feel free to drop me a line below!</h2>
+                <form className="contact-form" name="contact" method="post">
+                <input type="hidden" name="form-name" value="contact" /> 
+                    <field>
+                        <label>Name</label>
+                        <input 
+                            name="name" 
+                            type="text"
+                        />
+                    </field>
+                    <field>
+                        <label>Email</label>
+                        <input 
+                            name="email" 
+                            type="email" 
+                        />
+                    </field>
+                    <field>
+                        <label>Message</label>
+                        <textarea 
+                            name="message" 
+                            rows="8"
+                        />
+                    </field>
+                    <input className="submitButton" type="submit" value="Submit"/>
+                </form>
+            </div>
         </section>
     )
 }

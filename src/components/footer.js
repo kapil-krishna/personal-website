c29e63f42698a64d27dbe86202eb9627d74801e2
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-04-06 17:35:37 +0100

    Footer complete

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import LinkedInSVG from '../images/SVGs/iconmonstr-linkedin-4.svg';
+import GitHubSVG from '../images/SVGs/iconmonstr-github-1.svg';
+import EmailSVG from '../images/SVGs/iconmonstr-email-10.svg';
 
 const Footer = () => {
     return (
         <footer>
-            <h1>FOOTER</h1>
+            <div className="svgBox">
+                <a href="https://www.linkedin.com/in/kapil-krishna-profile/" target="_blank">
+                    <img src={LinkedInSVG}/>
+                </a>
+                <a href="https://github.com/kapil-krishna" target="_blank">
+                    <img src={GitHubSVG}/>
+                </a>
+                <a href="mailto:kapil.krishna.yo@gmail.com" target="_blank">
+                    <img src={EmailSVG}/>
+                </a>
+            </div>
+            <h4 className="copyright">
+                Copyright © 2020 Kapil Krishna.
+            </h4>
         </footer>
     )
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -7,8 +7,7 @@ const Projects = () => {
         <section className="projects-section" id="projects">
             <h1>PROJECTS</h1>
                 <Javaswitch />
-                <h2>More to come!</h2>
-
+                <h2 className="more-to-come">More to come!</h2>
         </section>
     )
 }

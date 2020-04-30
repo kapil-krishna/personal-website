2b47ef7695bb4e9316000207e8ccd63eee363f0a
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-05-01 00:41:59 +0100

    Added main photo on banner

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,6 +5,7 @@ const Home = () => {
     return (
         <section className="home-section" id="home">
             <h1 className="first-line">Hey there! I'm Kapil</h1>
+            <img src="https://drive.google.com/uc?id=1EAhskodqwfQkPHi0wIBnEFZlBZtqd8bn"/>
             <h1 className="second-line">Welcome to my website</h1>
         </section>
     )

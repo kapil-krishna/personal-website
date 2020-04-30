1524506f18b58740b83c24799c8d748766c71a31
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-04-30 17:07:43 +0100

    Minor copy change

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,7 +10,7 @@ const About = () => {
                 <img src={profilePhoto}/>
                 <div className="about-content">
                     <h2>"There's a difference between knowing the path and walking the path"</h2>
-                    <p>Hey there! I'm Kapil, an aspiring software developer from New Zealand, currently based in London. As a tech enthusiast who's always been curious about software engineering, recently I decided to take the leap and try to forge out a career doing something I love. So this is my first attempt at a personal website. Take a look around and let me know what you think!</p>
+                    <p>How's it goin'? I'm Kapil, an aspiring software developer from New Zealand, currently based in London. As a tech enthusiast who's always been curious about software engineering, recently I decided to take the leap and try to forge out a career doing something I love. So this is my first attempt at a personal website. Take a look around and let me know what you think!</p>
                     <p>When I'm not involved in tech, you can usually find me playing the guitar or annoying my girlfriend. Two of life's greatest pleasures.</p>
                 </div>
             </div>

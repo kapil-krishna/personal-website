bcc7fea46cdc889d51912c2f0dbce9a00b332449
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-06-12 21:23:36 +0100

    Simple animation for home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,7 +4,9 @@ import './home.scss';
 const Home = () => {
     return (
         <section className="home-section" id="home">
-            <h1 className="first-line">Hey there! I'm Kapil</h1>
+            <h1 className="first-line">
+                <span className="hey-there">Hey there! </span>
+                <span className="im-kapil">I'm Kapil</span></h1>
             <img src="https://drive.google.com/uc?id=1EAhskodqwfQkPHi0wIBnEFZlBZtqd8bn" alt="main-banner"/>
             <h2 className="second-line">Welcome to my website</h2>
         </section>
